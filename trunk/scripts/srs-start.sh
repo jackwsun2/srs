5eb3e4867381771bdab0719ec326f65ced8215f1
jack w. sun <sunjw@archempower.com>
2024-09-18 17:08:28 +0800

    添加自启动入口脚本

---
+++ trunk/scripts/srs-start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# gnome-terminal -- bash -c "~/Projects/srs/trunk/scripts/srs-start.sh"
+
+WORK_DIR=`pwd`
+SCRIPTS_DIR=$(cd $(dirname $0); pwd)
+
+sleep 3
+# sudo apt-get install xdotool
+xdotool windowminimize $(xdotool getactivewindow)
+sleep 3
+xdotool key esc
+xdotool mousemove 800 679
+xdotool click 1
+sleep 3
+
+cd $SCRIPTS_DIR/..
+objs/srs -c conf/rtmp2rtc.conf
