23cd1dbaff4b860f6d0f95d4567898df86bda6a9
jack w. sun <sunjw@archempower.com>
2024-09-19 10:15:06 +0800

    修改启动入口脚本

--- trunk/scripts/srs-start.sh
+++ trunk/scripts/srs-start.sh
@@ -9,10 +9,11 @@ sleep 3
 # sudo apt-get install xdotool
 xdotool windowminimize $(xdotool getactivewindow)
 sleep 3
-xdotool key esc
+# xdotool key esc
 xdotool mousemove 800 679
 xdotool click 1
 sleep 3
 
-cd $SCRIPTS_DIR/..
+cd $SCRIPTS_DIR
+cd ..
 objs/srs -c conf/rtmp2rtc.conf
